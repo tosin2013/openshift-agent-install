45d7924e6a65300634f7f2488378f55dd37f80c2
Tosin Akinosho <takinosh@redhat.com>
2024-07-07 09:32:28 -0400

    chore: Update download-openshift-cli.sh to dynamically determine the OpenShift CLI version based on the RHEL version

--- download-openshift-cli.sh
+++ download-openshift-cli.sh
@@ -3,13 +3,22 @@
 mkdir -p ./bin
 cd ./bin
 
-wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz
+# Determine the RHEL version
+rhel_version=$(rpm -E %{rhel})
 
+if [ "$rhel_version" -eq 8 ]; then
+    oc_version="stable-4.15"
+else
+    oc_version="stable-4.16"
+fi
+
+# Download and extract OpenShift CLI
+wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/$oc_version/openshift-client-linux.tar.gz
 tar zxvf openshift-client-linux.tar.gz
 rm -f openshift-client-linux.tar.gz
 
-wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-install-linux.tar.gz
-
+# Download and extract OpenShift Installer
+wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/$oc_version/openshift-install-linux.tar.gz
 tar zxvf openshift-install-linux.tar.gz
 rm -f openshift-install-linux.tar.gz
 
